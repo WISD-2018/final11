b7feede4737f49eb1b284d012e00ba6256a76a72
3A532027 <s3a532027@student.ncut.edu.tw>
2019-01-02 16:47:01 +0800

    訂單明細資料表

---
+++ database/migrations/2019_01_02_084456_create_orderdetail_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderdetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orderdetail', function (Blueprint $table) {
+            $table->increments('orderid');
+            $table->integer('goodid');
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orderdetail');
+    }
+}
