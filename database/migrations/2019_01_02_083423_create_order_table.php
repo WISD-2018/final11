51ba8ab50b66bef4346bb58c9658fff49349099b
3A532027 <s3a532027@student.ncut.edu.tw>
2019-01-02 16:44:15 +0800

    訂單資料表

---
+++ database/migrations/2019_01_02_083423_create_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->increments('orderid');
+            $table->integer('goodid');
+            $table->integer('userid');
+            $table->string('username');
+            $table->string('useremail');
+            $table->string('useraddress');
+            $table->dateTime('shipdate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order');
+    }
+}
