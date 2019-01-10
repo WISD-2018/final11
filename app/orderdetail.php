524c1119c2a2a54c8913c88e23f8346bcd929c8b
3A532049 <shelly213334@gmail.com>
2019-01-10 10:43:01 +0800

    產品model增加與修改

--- app/orderdetail.php
+++ app/orderdetail.php
@@ -7,9 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class orderdetail extends Model
 {
     protected $table = 'orderdetail';
+
     public function good(){
         return $this -> hasMany('app\good');
     }
+
+    public function good2(){
+        return $this -> hasMany('app\good2');
+    }
+
+    public function good3(){
+        return $this -> hasMany('app\good3');
+    }
+
     public function order(){
         return $this -> belongsTo('app\order');
     }
