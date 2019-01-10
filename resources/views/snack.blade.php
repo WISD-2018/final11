b0ffcc299a1ae513c2c32cf755542a2541142002
3a532008 <isme1788@gmail.com>
2019-01-10 19:53:05 +0800

    零食產品連資料庫

--- resources/views/snack.blade.php
+++ resources/views/snack.blade.php
@@ -11,10 +11,10 @@
     <title>狗糧福利社-零食</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="css/4-col-portfolio.css" rel="stylesheet">
+    <link href="../css/4-col-portfolio.css" rel="stylesheet">
 
   </head>
 
@@ -59,100 +59,25 @@
       <h1 class="my-4">分類>>>
         <small>零食</small>
       </h1>
-
+      @foreach ($goods as $good)
       <div class="row">
         <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
           <div class="card h-100">
             <a href="#"><img class="card-img-top" src="https://img.shop.com/Image/260000/264800/264853/products/1715704876__400x400__.jpg" alt=""></a>
             <div class="card-body">
               <h4 class="card-title">
-                <a href="{{ route('snackp1') }}">手作純肉乾</a>
+                <a href="{{ route('snack.detail',['id'=>$good->id]) }}">{{$good->goodname}}</a>
               </h4>
-              <p class="card-text"><li>番茄牛肉切條</li>
-                <li>鱉蛋雞肉條</li>
-                <li>蔓越莓雞肉切條</li>
-                <li>起司雞肉方塊</li>
-                <li>蘋果牛肉方塊</li></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="https://img13.360buyimg.com/n1/jfs/t1/973/23/9607/303561/5bac5ccaE6cd60300/6449aca6a0ff95bd.jpg" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="{{ route('snackp2') }}">天然手作零食-雞肉脆片100g</a>
-              </h4>
-              <p class="card-text">
-                <li>100%純天然肉品</li>
-                <li>絕不添加任何添加物</li>
-                <li>手工處理食材</li>
-                <li>去除多餘脂肪、碎骨頭</li>
-                <li>低溫烘焙鎖住養分</li>
-                <li>適用於：犬、貓</li></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="https://image.suning.cn/uimg/b2c/newcatentries/0070144353-000000000167574468_2.jpg_800w_800h_4e" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="{{ route('snackp3') }}">烘培點心 水果夾心餅/迷你潔牙餅/蔬菜起司餅 500G</a>
-              </h4>
-              <p class="card-text">
-                <li>富含豐富鈣質</li>
-                <li>補充Omega 3及6脂肪酸</li>
-                <li>天然維他命E及大量纖維質</li>
-                <li>礦物質及天然抗氧化劑</li>
-                <li>低卡路里輕食無負擔</li>
-                </p>
-                </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-          <div class="card h-100">
-            <a href="#"><img class="card-img-top" src="https://b.ecimg.tw/items/DEAK00A84176109/000001_1545789296.jpg" alt=""></a>
-            <div class="card-body">
-              <h4 class="card-title">
-                <a href="{{ route('snackp4') }}">多效雙刷頭潔牙骨 家庭號桶裝 1400g</a>
-              </h4>
-              <p class="card-text">
-                <li>食用級安心配方</li>
-                <li>維持口腔健康環境</li>
-                <li>有效降低口腔異味</li></p>
+              <p class="card-text"><li>{{$good->gooddetail}}</p>
             </div>
           </div>
         </div>
         </div>
       </div>
       <!-- /.row -->
-
+    @endforeach
       <!-- Pagination -->
-      <ul class="pagination justify-content-center">
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Previous">
-            <span aria-hidden="true">&laquo;</span>
-            <span class="sr-only">Previous</span>
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">1</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#" aria-label="Next">
-            <span aria-hidden="true">&raquo;</span>
-            <span class="sr-only">Next</span>
-          </a>
-        </li>
-      </ul>
+
 
     </div>
     <!-- /.container -->
