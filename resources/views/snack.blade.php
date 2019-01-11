a779b0e19ec8182669ca67136b68136c41472648
3A532027 <s3a532027@student.ncut.edu.tw>
2019-01-11 15:20:40 +0800

    123

--- resources/views/snack.blade.php
+++ resources/views/snack.blade.php
@@ -28,8 +28,8 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive" style="float: right;" >
-          <form method="get" action="{{ route('snack') }}">
+        <div class="collapse navbar-collapse" id="navbarResponsive" >
+          <form method="get" action="{{ route('snack') }}" >
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
               <a class="nav-link" href="/">
