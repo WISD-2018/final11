0fb842868f309e8ecc99bb05e0fa6ad729a4cfa6
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-15 22:36:03 +0800

    後端訂單

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class OrderController extends Controller
+{
+    public function Order()
+{
+    $data = DB::table('order')->get();;
+    return view('back.order', ['orders' => $data]);
+}
+    
+    public function member()
+    {
+        return view('back.member');
+    }
+    public function back()
+    {
+        return view('back.backindex');
+    }
+}
