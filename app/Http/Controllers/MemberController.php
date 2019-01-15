943acbeb7d08f38ccab8304ecd6c12e06c4117b9
3A532027 <s3a532027@student.nuct.edu.tw>
2019-01-15 22:35:53 +0800

    後端會員

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class MemberController extends Controller
+{
+    public function member()
+{
+    $data = DB::table('users')->get();;
+    return view('back.member', ['users' => $data]);
+}
+    public function back()
+    {
+        return view('back.backindex');
+    }
+    public function order()
+    {
+        return view('back.order');
+    }
+}
